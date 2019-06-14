452689be3cc300ab1af2470ae8c560ceb9f74f0b
eaglesakura <eagle.sakura@gmail.com>
2019-06-14 16:40:32 +0900

    chg: DelegateChannel<T> class is deprecated

--- src/main/kotlin/com/eaglesakura/armyknife/runtime/coroutines/DelegateChannel.kt
+++ src/main/kotlin/com/eaglesakura/armyknife/runtime/coroutines/DelegateChannel.kt
@@ -1,11 +1,6 @@
 package com.eaglesakura.armyknife.runtime.coroutines
 
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.ChannelIterator
-import kotlinx.coroutines.channels.SendChannel
-import kotlinx.coroutines.selects.SelectClause1
-import kotlinx.coroutines.selects.SelectClause2
 
 /**
  * Delegate supported channel.
@@ -15,54 +10,8 @@ import kotlinx.coroutines.selects.SelectClause2
  * @author @eaglesakura
  * @link https://github.com/eaglesakura/army-knife
  */
+@Deprecated("not necessary this class, Use to 'delegate' class with Kotlin language.")
 abstract class DelegateChannel<T>(
     @Suppress("MemberVisibilityCanBePrivate")
     protected val origin: Channel<T>
-) :
-    Channel<T> {
-    override val isClosedForReceive: Boolean
-        get() = origin.isClosedForReceive
-
-    override val isClosedForSend: Boolean
-        get() = origin.isClosedForSend
-
-    override val isEmpty: Boolean
-        get() = origin.isEmpty
-
-    override val isFull: Boolean
-        get() = false
-
-    override val onReceive: SelectClause1<T>
-        get() = origin.onReceive
-
-    override val onReceiveOrNull: SelectClause1<T?>
-        get() = origin.onReceiveOrNull
-
-    override val onSend: SelectClause2<T, SendChannel<T>>
-        get() = origin.onSend
-
-    override fun cancel() = origin.cancel()
-
-    override fun cancel(cause: CancellationException?) = origin.cancel(cause)
-
-    override fun cancel(cause: Throwable?): Boolean {
-        cancel(cause as? CancellationException)
-        return true
-    }
-
-    override fun close(cause: Throwable?): Boolean = origin.close(cause)
-
-    override fun invokeOnClose(handler: (cause: Throwable?) -> Unit) = origin.invokeOnClose(handler)
-
-    override fun iterator(): ChannelIterator<T> = origin.iterator()
-
-    override fun offer(element: T): Boolean = origin.offer(element)
-
-    override fun poll(): T? = origin.poll()
-
-    override suspend fun receive(): T = origin.receive()
-
-    override suspend fun receiveOrNull(): T? = origin.receiveOrNull()
-
-    override suspend fun send(element: T) = origin.send(element)
-}
+) : Channel<T> by origin
